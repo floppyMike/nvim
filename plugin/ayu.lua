010cc1f0ad38090a0bbad7ca23a95550965b5cb9
floppyMike <36990990+floppyMike@users.noreply.github.com>
2025-03-11 23:06:15 +0100

    Add ayu

---
+++ plugin/ayu.lua
@@ -0,0 +1,10 @@
+require"pluginmanager".ensure("Shatur", "neovim-ayu", {})
+
+require'ayu'.setup {
+	overrides = {
+		LineNrAbove = { fg = '#51B3EC' },
+		LineNrBelow = { fg = '#FB508F' },
+	},
+}
+
+vim.cmd [[colorscheme ayu]]
