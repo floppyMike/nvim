5ce33a128dc0086b6e0fcf35b407a52e6e65ba84
floppyMike <36990990+floppyMike@users.noreply.github.com>
2025-03-19 17:08:13 +0100

    Remove ftplugins

--- ftplugin/scala.lua
+++
@@ -1,9 +0,0 @@
-local config = require("metals").bare_config()
-
-config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-config.on_attach = function(client, bufnr)
-	On_attach(client, bufnr)
-end
-
-require("metals").initialize_or_attach(config)
