35f5c3e34fb96bc540a953ca43f6af6faa7587d4
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-03-01 19:17:24 +0100

    feat: ice upgrades

--- colors/ice.lua
+++ colors/ice.lua
@@ -3,6 +3,7 @@ if vim.g.colors_name then
 end
 
 vim.g.colors_name = "ice"
+vim.o.background = "dark"
 
 local c = {
 	white = '#FFFFFF',
@@ -10,7 +11,7 @@ local c = {
 
 	lsp_inlay_hint = '#969696',
 
-	accent = '#59C2FF',
+	accent = '#2ab1ff',
 	bg = '#000000',
 	fg = '#dddddd',
 
@@ -18,7 +19,7 @@ local c = {
 
 	tag = '#dddddd',
 	func = '#65d1ff', -- functions
-	entity = '#59C2FF', -- Types
+	entity = '#2ab1ff', -- Types
 	string = '#c6efff',
 	regexp = '#95E6CB',
 	markup = '#d1e3ff',
@@ -34,21 +35,18 @@ local c = {
 	panel_shadow = '#05070A',
 	panel_border = '#000000',
 	gutter_normal = '#454B55',
-	gutter_active = '#626975',
 	selection_inactive = '#122132',
-	selection_border = '#304357',
-	guide_active = '#3C414A',
-	guide_normal = '#1E222A',
+	guide_normal = '#555555',
 
 	vcs_added = '#7FD962',
 	vcs_modified = '#73B8FF',
 	vcs_removed = '#F26D78',
 
-	error = '#D95757',
-	warning = '#FF8F40',
-
 	vcs_added_bg = '#1D2214',
 	vcs_removed_bg = '#2D2220',
+
+	error = '#D95757',
+	warning = '#FF8F40',
 
 	fg_idle = '#565B66',
 }
