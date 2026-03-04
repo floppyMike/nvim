a886e3be38947d59ef410b572dad3ca5939d7391
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-03-04 21:44:00 +0100

    feat: LSP bindings

--- init.lua
+++ init.lua
@@ -80,8 +80,8 @@ local keymap = {
 	PrevBuffer = "<a-B>",
 	NextQuickfix = "<a-q>",
 	PrevQuickfix = "<a-Q>",
-	NextLocation = "<a-l>",
-	PrevLocation = "<a-L>",
+	NextLocation = "<a-o>",
+	PrevLocation = "<a-O>",
 	NextParagraph = "<a-p>",
 	PrevParagraph = "<a-P>",
 	NextDiagnostic = "<a-d>",
@@ -158,9 +158,16 @@ local keymap = {
 	GitReset = "<leader>hr",
 
 	-- LSP
-	LSPDefinition = "grd",
+	LSPDefinition = "gd",
+	LSPDeclaration = "gD",
+	LSPRename = "gr",
+	LSPReferences = "gR",
+	LSPImplementation = "gi",
+	LSPHover = "K",
+	LSPCodeAction = "ga",
+	LSPSymbolPick = "gs",
+	LSPLineProblems = "P",
 	LSPFormat = "<a-i>",
-	LSPSymbolPick = "gO",
 
 	-- Text Movement
 	MoveLeft = "<a-h>",
@@ -315,13 +322,6 @@ end, { desc = "Set spellcheck to german" })
 require 'indent-o-matic'.setup {}
 
 --
--- LSP Management
---
-
-require "mason".setup()
-require "mason-lspconfig".setup()
-
---
 -- Completion
 --
 
@@ -387,16 +387,31 @@ vim.keymap.set('n', keymap.DuckDestroyAll, function() require("duck").cook_all()
 -- LSP Configuration
 --
 
-vim.keymap.set('n', keymap.LSPDefinition, vim.lsp.buf.definition, { desc = "Goto definition" })
-vim.keymap.set('n', keymap.LSPFormat, vim.lsp.buf.format, { desc = "Format document" })
-vim.keymap.set("n", keymap.LSPSymbolPick, function() MiniExtra.pickers.lsp({ scope = "document_symbol" }) end)
+require "mason".setup()
+require "mason-lspconfig".setup()
+
+vim.keymap.set('n', keymap.LSPDefinition, vim.lsp.buf.definition, { desc = "Goto definition", nowait = true })
+vim.keymap.set('n', keymap.LSPFormat, vim.lsp.buf.format, { desc = "Format document", nowait = true })
+vim.keymap.set("n", keymap.LSPCodeAction, vim.lsp.buf.code_action, { desc = "Perform a code action", nowait = true })
+vim.keymap.set("n", keymap.LSPDeclaration, vim.lsp.buf.declaration, { desc = "Goto declaration", nowait = true })
+vim.keymap.set("n", keymap.LSPHover, vim.lsp.buf.hover, { desc = "Show LSP into for word under cursor", nowait = true })
+vim.keymap.set("n", keymap.LSPImplementation, vim.lsp.buf.implementation, { desc = "Goto implementation", nowait = true })
+vim.keymap.set("n", keymap.LSPRename, vim.lsp.buf.rename, { desc = "Rename a LSP symbol", nowait = true })
+
+vim.keymap.set("n", keymap.LSPLineProblems, vim.diagnostic.open_float,
+	{ desc = "Show diagnostics for the line", nowait = true })
+
+vim.keymap.set("n", keymap.LSPReferences, function() MiniExtra.pickers.lsp({ scope = "references" }) end,
+	{ desc = "Show all references of a LSP symbol", nowait = true })
+
+vim.keymap.set("n", keymap.LSPSymbolPick, function() MiniExtra.pickers.lsp({ scope = "document_symbol" }) end,
+	{ desc = "Pick out a workspace symbol and jump to it" })
 
 vim.keymap.set({ "n", "x" }, keymap.NextDiagnostic, function() vim.diagnostic.jump { count = 1, float = true } end,
 	{ desc = "Move to next Diagnostic" })
 
 vim.keymap.set({ "n", "x" }, keymap.PrevDiagnostic, function() vim.diagnostic.jump { count = -1, float = true } end,
 	{ desc = "Move to previous Diagnostic" })
-
 
 vim.lsp.config("lua_ls", {
 	on_init = function(client)
