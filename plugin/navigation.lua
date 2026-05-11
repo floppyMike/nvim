9815140e76da2622686893bf600a6de332ddacff
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-05-11 16:47:17 +0200

    feat(nav): tab navigation

--- plugin/navigation.lua
+++ plugin/navigation.lua
@@ -20,10 +20,16 @@ vim.keymap.set("n", keymap.NextLocation, "<cmd>lnext<cr>", { desc = "Move to nex
 vim.keymap.set("n", keymap.PrevLocation, "<cmd>lprevious<cr>", { desc = "Move to previous location list item" })
 vim.keymap.set({ "n", "v" }, keymap.NextParagraph, "}", { desc = "Move to next paragraph" })
 vim.keymap.set({ "n", "v" }, keymap.PrevParagraph, "{", { desc = "Move to previous paragraph" })
+
 vim.keymap.set("n", "<esc>", "<cmd>noh<CR>", { desc = "Stop search with ESC" })
+
 vim.keymap.set("n", keymap.FilePick, MiniPick.builtin.files)
 vim.keymap.set("n", keymap.GrepPick, MiniPick.builtin.grep)
 vim.keymap.set("n", keymap.BufferPick, MiniPick.builtin.buffers)
+
+for i = 1, 9 do
+	vim.keymap.set("n", "<a-" .. i .. ">", i .. "gt", { desc = "Go to tab " .. i })
+end
 
 vim.keymap.set("n", keymap.JumpWord, function()
 	local builtin = Jump2d.builtin_opts.word_start
