133567c66584c09e8da3788656c245bbfaa0f637
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-05-11 10:57:23 +0200

    feat(git): add additional git status commands

--- plugin/git.lua
+++ plugin/git.lua
@@ -17,6 +17,24 @@ require "mini.diff".setup {
 	},
 }
 
+local reset_line = function(ev, l)
+	local rel = l:match("([^%s]+)%s*$")
+	vim.cmd("Git restore " .. vim.fn.fnameescape(vim.fs.joinpath(ev.data.cwd, rel)))
+	vim.notify("Reset " .. rel, vim.log.levels.INFO)
+end
+
+local stage_line = function(ev, l)
+	local rel = l:match("([^%s]+)%s*$")
+	vim.cmd("Git add " .. vim.fn.fnameescape(vim.fs.joinpath(ev.data.cwd, rel)))
+	vim.notify("Staged " .. rel, vim.log.levels.INFO)
+end
+
+local unstage_line = function(ev, l)
+	local rel = l:match("([^%s]+)%s*$")
+	vim.cmd("Git restore --staged " .. vim.fn.fnameescape(vim.fs.joinpath(ev.data.cwd, rel)))
+	vim.notify("Unstaged " .. rel, vim.log.levels.INFO)
+end
+
 vim.api.nvim_create_autocmd('User', {
 	pattern = 'MiniGitCommandSplit',
 	callback = function(ev)
@@ -42,9 +60,7 @@ vim.api.nvim_create_autocmd('User', {
 
 		vim.keymap.set('v', 'a', function()
 			for _, l in ipairs(vim.fn.getline("'<", "'>")) do
-				local rel = l:match("([^%s]+)%s*$")
-				vim.cmd("Git add " .. vim.fn.fnameescape(vim.fs.joinpath(ev.data.cwd, rel)))
-				vim.notify("Staged " .. rel, vim.log.levels.INFO)
+				stage_line(ev, l)
 			end
 		end, {
 			buffer = true,
@@ -52,12 +68,42 @@ vim.api.nvim_create_autocmd('User', {
 		})
 
 		vim.keymap.set('n', 'a', function()
-			local rel = vim.api.nvim_get_current_line():match("([^%s]+)%s*$")
-			vim.cmd("Git add " .. vim.fn.fnameescape(vim.fs.joinpath(ev.data.cwd, rel)))
-			vim.notify("Staged " .. rel, vim.log.levels.INFO)
+			stage_line(ev, vim.api.nvim_get_current_line())
 		end, {
 			buffer = true,
 			desc = 'Stage path',
+		})
+
+		vim.keymap.set('v', 'D', function()
+			for _, l in ipairs(vim.fn.getline("'<", "'>")) do
+				reset_line(ev, l)
+			end
+		end, {
+			buffer = true,
+			desc = 'Reset paths',
+		})
+
+		vim.keymap.set('n', 'D', function()
+			reset_line(ev, vim.api.nvim_get_current_line())
+		end, {
+			buffer = true,
+			desc = 'Reset path',
+		})
+
+		vim.keymap.set('v', 'd', function()
+			for _, l in ipairs(vim.fn.getline("'<", "'>")) do
+				unstage_line(ev, l)
+			end
+		end, {
+			buffer = true,
+			desc = 'Unstage paths',
+		})
+
+		vim.keymap.set('n', 'd', function()
+			unstage_line(ev, vim.api.nvim_get_current_line())
+		end, {
+			buffer = true,
+			desc = 'UnStage path',
 		})
 	end,
 })
