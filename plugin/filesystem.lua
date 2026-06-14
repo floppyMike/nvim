c76d06454da0373fdb6a9d5b211e8d50b651ec2b
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-06-14 20:05:22 +0200

    feat: Minifile current filesystem

--- plugin/filesystem.lua
+++ plugin/filesystem.lua
@@ -1,25 +1,5 @@
 require "mini.files".setup()
-vim.keymap.set("n", keymap.Filesytem, MiniFiles.open)
-
-vim.api.nvim_create_autocmd("User", {
-	pattern = "MiniFilesBufferCreate",
-	callback = function(args)
-		local buf = args.data.buf_id
-
-		vim.keymap.set('n', 'ge', function()
-			local entry = MiniFiles.get_fs_entry()
-			if not entry then return vim.notify("Invalid item", vim.log.levels.ERROR) end
-
-			vim.system({"extract", entry.name}, { cwd = vim.fs.dirname(entry.path) }, function(obj)
-				vim.schedule(function()
-					if obj.code == 0 then
-						vim.notify("Extracted: " .. entry.name, vim.log.levels.INFO)
-						MiniFiles.refresh()
-					else
-						vim.notify("Extraction failed: " .. (obj.stderr or "error"), vim.log.levels.ERROR)
-					end
-				end)
-			end)
-		end, { buffer = buf, desc = 'Extract archive' })
-	end,
-})
+vim.keymap.set("n", keymap.Filesytem, function()
+	MiniFiles.open(vim.api.nvim_buf_get_name(0), true)
+	MiniFiles.reveal_cwd()
+end)
